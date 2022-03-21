56480d30748fc1e4bd7d0ffa7e693e1d288e8246
ephraim888sun <ephraim888sun@gmail.com>
2022-03-21 17:34:10 -0500

    add components

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './contact.css'
+import Phone from "../../img/phone.png"
+import Email from "../../img/email.png"
+import Address from "../../img/address.png"
+
+const Contact = () => {
+  return (
+    <div className='c'>
+      <div className="c-bg">
+        <div className="c-wrapper">
+          <div className="c-left">
+            <h1 className="c-title">Let's discuss your project</h1>
+            <div className="c-info">
+              <div className="c-info-item">
+                <img src={Phone} alt="" className="c-icon" />
+                +1 1234 556 75
+              </div>
+              <div className="c-info-item">
+                <img className="c-icon" src={Email} alt="" />
+                contact@lama.dev
+              </div>
+              <div className="c-info-item">
+                <img className="c-icon" src={Address} alt="" />
+                245 King Street, Touterie Victoria 8520 Australia
+              </div>
+            </div>
+          </div>
+
+          <div className="c-right"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://cdn.pixabay.com/photo/2018/01/06/07/53/social-3064515_960_720.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2019/09/09/08/23/internet-4463031__340.jpg",
+    link: "https://github.com/ephraim888sun",
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+    link: "https://github.com/ephraim888sun",
+  },
+  {
+    id: 4,
+    img: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+    link: "https://github.com/ephraim888sun",
+  },
+  {
+    id: 5,
+    img: "https://www.searchenginejournal.com/wp-content/uploads/2018/10/How-to-Boost-Your-Images%E2%80%99-Visibility-on-Google-Images-760x400.png",
+    link: "https://github.com/ephraim888sun",
+  },
+  {
+    id: 6,
+    img: "https://cdn.theatlantic.com/media/img/photo/2018/10/images-of-the-season-fall-is-in-the/f02_RTX6EJJJ-1/original.jpg",
+    link: "https://github.com/ephraim888sun",
+  },
+];
