9fcea0a28bffaa54c430c6cf4e36701dee3f9e20
ephraim888sun <ephraim888sun@gmail.com>
2022-03-26 12:33:00 -0500

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Intro from './components/intro/Intro';
 import About from './components/about/About';
 import ProductList from './components/productList/ProductList';
 import Contact from './components/contact/Contact';
 import Toggle from './components/toggle/Toggle';
-import { ThemeContext } from './context';
 
 function App() {
-
-  const theme = useContext(ThemeContext)
-  const blueMode = theme.state.blueMode;
 
   return (
     <div>
